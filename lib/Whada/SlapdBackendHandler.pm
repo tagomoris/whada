fa65f3cfe4bd2df2f91ccc325b02314997615d1b
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-09-22 18:35:47 +0900

    import actual search-handler-logic from LDWhada::SlapdBackendHandler

--- lib/Whada/SlapdBackendHandler.pm
+++ lib/Whada/SlapdBackendHandler.pm
@@ -3,18 +3,42 @@ package Whada::SlapdBackendHandler;
 use strict;
 use warnings;
 use Carp;
+use MIME::Base64;
+use Try::Tiny;
 
-use Net::LDAP;
+use Whada::Logger;
+use Whada::Engine;
 
 sub new {
     my $class = shift;
-    return bless {config => {}}, $class;
+    return bless {
+        converter => 'Whada::Converter', # MUST be overwritten
+        dictionary => 'Whada::Dictionary', # MUST be overwritten
+        default_privilege => 'denied', # MUST be specified explicitly in subclass
+        global_config => {},
+        config => {}
+    }, $class;
+}
+
+sub attribute_dump {
+    my ($attr, $value) = @_;
+    if ($attr =~ /\Aobject(s|gu)id\Z/i) {
+        my $encoded = encode_base64($value);
+        chomp $encoded;
+        return $attr . ': ' . $encoded;
+    }
+    return $attr . ': ' . $value;
+}
+
+sub configurations {
+    my $this = shift;
+    return {%{$this->{global_config}}, %{$this->{config}}};
 }
 
 sub config {
     my $this = shift;
     my ($param, $value) = @_;
-    return 0 if $param !~ /^wadaBackend(.+)$/;
+    return 0 if $param !~ /^whadaBackend(.+)$/;
 
     $this->{config}->{lc($1)} = $value;
     return 0;
@@ -25,7 +49,27 @@ sub init {
 }
 
 sub search {
-    croak "not implemented";
+    my $this = shift;
+    my ($base, $scope, $deref, $sizeLim, $timeLim, $filterStr, $attrOnly, @attrs ) = @_;
+
+    # $filterStr: (&(objectClass=*)(uid=tagomoris)), (&(objectClass=*)(MAIL=tagomoris@livedoor.jp))
+    my $entry;
+    try {
+        my $config = $this->configurations;
+        $entry = Whada::Engine->authorize({
+            credential => ($this->{converter})->new({ldapquery => {base => $base, filter => $filterStr}})->credential(),
+            dictionary => ($this->{dictionary})->new($config),
+            logger => Whada::Logger->new('slapd', $config->{logpath}),
+            default_privilege => $this->{default_privilege},
+        });
+    } catch {
+        print STDERR "perl backend search failed with error: $_\n";
+        $entry = undef;
+    };
+    return (0) unless defined $entry;
+    my $pairs = join("\n", map {attribute_dump($_, $entry->get_value($_))} $entry->attributes());
+    my $entryString = "dn: " . $entry->dn() . "\n" . $pairs . "\n";
+    return (0, $entryString);
 }
 
 sub compare {
@@ -33,6 +77,7 @@ sub compare {
 }
 
 sub bind {
+    # TODO write
     my $this = shift;
     # you cannot bind with virtual entries.
     return 0;
