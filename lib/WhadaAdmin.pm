963758b7fa4db624645fe430a66f53bc7e45f280
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-10-06 23:38:24 +0900

    implement privilege/user data editing...

--- lib/WhadaAdmin.pm
+++ lib/WhadaAdmin.pm
@@ -15,6 +15,7 @@ use HTTP::Session::State::Cookie;
 use JSON;
 
 use Kossy;
+use Log::Minimal;
 
 use WhadaAdmin::Config;
 
@@ -188,7 +189,7 @@ post '/login' => [qw/check_authenticated/] => sub {
         $session->set('whada_privs', encode_json($privs));
         $session->set('username', $username);
         $session->set('is_admin', $is_admin);
-        $session->set('is_partial_admin', scalar(grep {$_ =~ /^WHADA\+.+\+ADMIN$/} keys(%$privs)) > 1);
+        $session->set('is_partial_admin', scalar(grep {$_ =~ /^WHADA\+ADMIN\+.+$/} keys(%$privs)) > 0);
     }
     else {
         $session->set('logged_in', 0);
@@ -218,13 +219,42 @@ get '/admin_privs' => [qw/require_authenticated_admin/] => sub {
 
 get '/priv/:privname' => [qw/require_authenticated/] => sub {
     my ($self, $c) = @_;
-    $c->render_json(Whada::PrivStore->priv_data($c->args->{labelname}));
-};
-
-post '/priv/create' => [qw/require_authenticated_admin/] => sub {
+    $c->render_json(Whada::PrivStore->priv_data($c->args->{privname}));
 };
 
 post '/priv/update' => [qw/require_authenticated_admin/] => sub {
+    my ($self, $c) = @_;
+    warnf ddf
+    my $privilege = uc($c->req->parameters->{privilege});
+    unless (Whada::PrivStore->priv_data($privilege)) {
+        $c->render_json({result => JSON::false, message => 'unknown privilege:' . $privilege});
+        return;
+    }
+    my $dst_type = $c->req->parameters->{dst_type};
+    unless (Whada::PrivStore->check_priv_type($dst_type)) {
+        warnf 'wrong privilege type:' . $dst_type;
+        $c->render_json({result => JSON::false, message => 'wrong privilege type:' . $dst_type});
+        return;
+    }
+    my $root_admin_priv = 'WHADA+ADMIN+' . (split(/\+/, $privilege))[0];
+    my $check = Whada::PrivStore->check(Whada::Credential->new({
+        username => $c->stash->{username},
+        privilege => $root_admin_priv,
+    }));
+    unless ($check) {
+        $c->render_json({result => JSON::false, message => 'permission denied: you do not have privilege:' . $root_admin_priv});
+        return;
+    }
+    try {
+        Whada::PrivStore->set_priv_type($privilege, $dst_type);
+        $c->render_json({result => JSON::true, message => 'privilege type changed:' . $dst_type});
+    } catch {
+        warnf 'failed to save privilege:' . $privilege . ', error:' . ddf($_);
+        $c->render_json({result => JSON::false, message => 'unknown error (maybe database error)'});
+    };
+};
+
+post '/priv/create' => [qw/require_authenticated_admin/] => sub {
 };
 
 post '/priv/drop' => [qw/require_authenticated_admin/] => sub {
@@ -240,13 +270,63 @@ get '/user/:username' => [qw/require_authenticated/] => sub {
     $c->render_json(Whada::PrivStore->priv_data($c->args->{username}));
 };
 
+post '/user/update' => [qw/require_authenticated_admin/] => sub {
+    my ($self, $c) = @_;
+    my $operation = $c->req->parameters->{operation};
+    unless ($operation eq 'allow' or $operation eq 'deny' or $operation eq 'remove') {
+        return $c->render_json({result => JSON::false, message => 'invalid operation:' . $operation});
+    }
+    my $target_username = $c->req->parameters->{username};
+    my $target_user_credential = Whada::Credential->new({username => $target_username});
+    my $target = Whada::PrivStore->user_data($target_username);
+    unless ($target->{privileges}) {
+        return $c->render_json({result => JSON::false, message => 'unknown user:' . $target_username});
+    }
+    my $privilege = uc($c->req->parameters->{privilege});
+    unless (Whada::PrivStore->priv_data($privilege)) {
+        return $c->render_json({result => JSON::false, message => 'unknown privilege:' . $privilege});
+    }
+    my $root_admin_priv = 'WHADA+ADMIN+' . (split(/\+/, $privilege))[0];
+    warnf ddf($c->stash);
+    my $check = Whada::PrivStore->check(Whada::Credential->new({
+        username => $c->stash->{username},
+        privilege => $root_admin_priv,
+    }));
+    unless ($check) {
+        return $c->render_json({result => JSON::false, message => 'permission denied: you do not have privilege:' . $root_admin_priv});
+    }
+    try {
+        if ($operation eq 'allow') {
+            Whada::PrivStore->allow_privileges($target_user_credential, $privilege);
+        }elsif ($operation eq 'deny') {
+            Whada::PrivStore->deny_privileges($target_user_credential, $privilege);
+        }elsif ($operation eq 'remove') {
+            delete $target->{privileges}->{$privilege};
+            Whada::PrivStore->save_user_data($target);
+        }
+        $c->render_json({result => JSON::true, message => 'user privilege successfully updated.'});
+    } catch {
+        warnf 'failed to save user data:' . $target_username . ', error:' . ddf($_);
+        $c->render_json({result => JSON::false, message => 'unknown error (maybe database error)'});
+    };
+};
+
 post '/user/create' => [qw/require_authenticated_admin/] => sub {
 };
 
-post '/user/update' => [qw/require_authenticated_admin/] => sub {
+post '/user/drop' => [qw/require_authenticated_admin/] => sub {
 };
 
-post '/user/drop' => [qw/require_authenticated_admin/] => sub {
+get '/check' => [qw/require_authenticated/] => sub {
+    my ($self, $c) = @_;
+    my $username = $c->req->parameters->{username};
+    my $privilege = $c->req->parameters->{privilege};
+    if (Whada::PrivStore->check(Whada::Credential->new({usename => $username, privilege => $privilege}))) {
+        $c->render_json({result => "Access allowed: $username -> $privilege"});
+    }
+    else {
+        $c->render_json({result => "Access denied: $username -> $privilege"});
+    }
 };
 
 1;
--- public/js/whada.js
+++ public/js/whada.js
@@ -1,12 +1,53 @@
 $(function(){
+  load_page();
+});
+
+function load_page() {
   load_privileges_list();
   load_users_list();
+
+  $('#checker_bar')
+    .unbind()
+    .click(function(){
+      $('.control #checker').slideToggle(50);
+    });
   $("a.checker_button").button();
+  $('a#checker_button')
+    .unbind()
+    .click(checker_execute);
+
   if (whada_logged_in_as_admin) {
     load_privileges_admin_list();
-    $("a.panel_button").button();
+
+    $("a.operator_button,a.panel_button").button();
+    $('#operator_bar')
+      .unbind()
+      .click(function(){
+        $('.control #operator').slideToggle(50);
+      });
+    $('.operator_button')
+      .unbind()
+      .click(operator_execute);
+
+    $('#panel_bar')
+      .unbind()
+      .click(function(){
+        $('.control #panel').slideToggle(50);
+      });
   }
-});
+};
+
+function reload_page() {
+  $('#privileges-list').empty();
+  $('#users-list').empty();
+  $("a.checker_button").button('destroy');
+
+  if (whada_logged_in_as_admin) {
+    $('#privileges-admin-list').empty();
+    $("a.operator_button,a.panel_button").button('destroy');
+  }
+  load_page();
+};
 
 function re_assign_events() {
   $('div.username,div.privilege')
@@ -15,9 +56,15 @@ function re_assign_events() {
       $(this).siblings('ul').slideToggle(80);
       if ($(this).hasClass('username')) {
         $('#checker_username').val($(this).text());
+        if (whada_logged_in_as_admin) {
+          $('#operator_user').text($(this).text());
+        }
       }
       if ($(this).hasClass('privilege')) {
         $('#checker_privilege').val($(this).text());
+        if (whada_logged_in_as_admin) {
+          $('#operator_priv').text($(this).text());
+        }
       }
       $('ul.items li')
         .unbind('hover')
@@ -25,8 +72,35 @@ function re_assign_events() {
           function() { $(this).addClass('ui-state-hover'); }, 
           function() { $(this).removeClass('ui-state-hover'); }
         );
+      $('ul.items li.operation_item')
+        .unbind('click')
+        .click(function(){
+          $('.control #operator').slideDown(50);
+          var username = $(this).closest('.items').siblings('.username').text();
+          var parts = $(this).text().split(':');
+          $('#operator_user').text(username);
+          $('#operator_priv').text(parts[0]);
+          $('#operator_status').text(parts[1]);
+          $('#checker_username').val(username);
+          $('#checker_privilege').val(parts[0]);
+        });
       return false;
     });
+};
+
+function show_dialog(title, message, buttons){
+  $('#dialog').attr('title', title);
+  $('#dialog_message').text(message);
+  $('#dialog').bind('dialogclose', function(event, ui){
+    $('#dialog').dialog('destroy');
+    $('#dialog').attr('title', '');
+    $('#dialog_message').text('');
+  });
+  $('#dialog').dialog({
+    autoOpen: true,
+    width: 400,
+    buttons: buttons
+  });
 };
 
 $.template("privilegeItemTemplate",
@@ -65,7 +139,7 @@ $.template("userItemTemplate",
            '  <div class="username" style="font-size: large; font-weight: bold;">${UserName}</div>' +
            '  <ul class="items ui-widget ui-helper-clearfix">' +
            '    {{each Privileges}}' +
-           '    <li class="ui-state-default ui-corner-all">${$value}</li>' +
+           '    <li class="operation_item ui-state-default ui-corner-all">${$value}</li>' +
            '    {{/each}}' +
            '  </ul>' +
            '</li>');
@@ -88,31 +162,47 @@ function load_users_list(){
   });
 };
 
-function create_queryitem_object(queryid, id_prefix){
-  var query = shibdata.query_cache[queryid];
-  if (! query)
-    return '';
-  var lastresult = query_last_result(query);
-  var executed_at = (lastresult && lastresult.executed_at) || '-';
-  var keyword_primary = (query.keywords && query.keywords.length > 0 && query.keywords[0]) || '-';
-  return {
-    QueryId: (id_prefix || '') + query.queryid,
-    Information: executed_at + ', ' + keyword_primary,
-    Statement: query.querystring,
-    Status: query_current_state(query),
-    Etc: timelabel_elapsed(lastresult.completed_at, lastresult.executed_at) +
-      ((lastresult && lastresult.bytes && lastresult.lines &&
-        (', ' + lastresult.bytes + ' bytes, ' + lastresult.lines + ' lines')) || '')
-  };
+function checker_execute(){
+  var username = $('#checker_username').val();
+  var privilege = $('#checker_privilege').val();
+  if (username.length < 1 || privilege.length < 1) {
+    show_dialog('Error', 'blank username or password', {"OK":function(){$('#dialog').dialog('close');}});
+    return;
+  }
+  $.get('/check', {username:username, privilege:privilege}, function(data){
+    show_dialog('Checker result', data['result'], {"OK":function(){$('#dialog').dialog('close');}});
+  });
 };
 
-function update_yours_tab(){
-  $('#tab-yours')
-    .empty()
-    .append('<div><h3><a href="#">your queries</a></h3><div id="yours-idlist"></div></div>');
-  if (execute_query_list().length > 0)
-    $.tmpl("queryItemTemplate",
-           execute_query_list().map(function(id){return create_queryitem_object(id, 'yours-');})
-          ).appendTo('#tab-yours div div#yours-idlist');
+function operator_execute(event){
+  var matched = /^operator_(.*)$/.exec($(event.target).closest('a').attr('id'));
+  var operation = matched[1];
+  var priv = $('#operator_priv').text();
+  if (priv.length < 1) {
+    show_dialog('Error', 'privilege not selected', {"OK":function(){$('#dialog').dialog('close');}});
+    return;
+  }
+  if (operation === 'always_allow' || operation === 'default_allow' || operation === 'default_deny' || operation === 'always_deny'){
+    $.post('/priv/update', {privilege:priv, dst_type:operation}, function(data){
+      if (data.result)
+        show_dialog('Success', data.message, {"OK":function(){$('#dialog').dialog('close'); reload_page();}});
+      else
+        show_dialog('Error', data.message, {"OK":function(){$('#dialog').dialog('close');}});
+    });
+    return;
+  }
+  else if (operation === 'allow' || operation === 'deny' || operation === 'remove') {
+    var username = $('#operator_user').text();
+    if (username.length < 1) {
+      show_dialog('Error', 'user not selected', {"OK":function(){$('#dialog').dialog('close');}});
+      return;
+    }
+    $.post('/user/update', {username:username, privilege:priv, operation:operation}, function(data){
+      if (data.result)
+        show_dialog('Success', data.message, {"OK":function(){$('#dialog').dialog('close'); reload_page();}});
+      else
+        show_dialog('Error', data.message, {"OK":function(){$('#dialog').dialog('close');}});
+    });
+    return;
+  }
 };
-
